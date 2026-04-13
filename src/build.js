e96afeb67d51f9fda41115e119790f3ea793b24e
Albert Puig <albert.puig@ext.zetes.com>
2026-04-13 20:12:04 +0200

    chore(seo): add Google Search Console verification file

--- src/build.js
+++ src/build.js
@@ -90,7 +90,7 @@ for (const { code } of LANGUAGES) {
 }
 
 // Copy static assets to dist/
-const STATIC_ASSETS = ['favicon.svg', 'og-image.png', 'robots.txt', 'sitemap.xml'];
+const STATIC_ASSETS = ['favicon.svg', 'og-image.png', 'robots.txt', 'sitemap.xml', 'googlebb0ee2a5551035cd.html'];
 for (const asset of STATIC_ASSETS) {
   const src = path.join(ROOT, asset);
   if (fs.existsSync(src)) {
